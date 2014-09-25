5a2cfedd714b905e5df5787b7df54535ea902f3e
René Kriegler <rk@rene-kriegler.de>
2014-09-25 21:30:36 +0200

    Adding JavaDoc for StateInfo methods in PerfectHashDictionary

--- src/main/java/eu/danieldk/dictomaton/PerfectHashDictionary.java
+++ src/main/java/eu/danieldk/dictomaton/PerfectHashDictionary.java
@@ -18,8 +18,26 @@ public interface PerfectHashDictionary extends Dictionary {
      */
     public int number(CharSequence seq);
 
+    /**
+     * Compute the state for traversing the automaton given CharSequence seq. The
+     * perfect hash code is available via {@link StateInfo#getHash()}. The traversal 
+     * can be resumed using {@link #getStateInfo(CharSequence, StateInfo)}
+     * 
+     * @param seq The sequence to compute the perfect hash value for.
+     * @return A StateInfo object containing the hash code and further information about the state
+     *         after the traversal for seq.
+     */
     public StateInfo getStateInfo(CharSequence seq);
 
+    /**
+     * Compute the state for traversing the automaton given CharSequence seq starting 
+     * at state startInfo. The perfect hash code is available via {@link StateInfo#getHash()}. 
+     * 
+     * @param seq The sequence to compute the perfect hash value for.
+     * @param startInfo The state from which to resume traversal or null for the inital state.
+     * @return  A StateInfo object containing the hash code and further information about the state
+     *          after the traversal for seq.
+     */
     public StateInfo getStateInfo(CharSequence seq, StateInfo startInfo);
     
     /**
